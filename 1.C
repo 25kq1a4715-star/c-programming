ed584b73ea0f725436249312a7516cb998eed38c
25kq1a4715-star <25kq1a4715@pace.ac.in>
2025-11-01 15:11:57 +0530

    Add files via upload

---
+++ 1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,addition,subtraction,multiplication,division;
+clrscr();
+printf("enter two values : \n");
+scanf("%d%d",&a,&b);
+addition = a+b;
+printf("addition : %d \n",addition);
+subtraction = a-b;
+printf("subtraction : %d \n",subtraction);
+multiplication=a*b;
+printf("multiplication : %d \n",multiplication);
+division = a/b;
+printf("division : %d \n",division);
+getch();
+}
